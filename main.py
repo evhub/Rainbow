87939572cb90d761b417deef2d0ed7cccd8fc64c
Evan Hubinger <evanjhub@gmail.com>
2020-01-21 14:37:30 -0800

    Make main work as module

--- main.py
+++ main.py
@@ -56,25 +56,6 @@ parser.add_argument('--checkpoint-interval', default=0, help='How often to check
 parser.add_argument('--memory', help='Path to save/load the memory from')
 parser.add_argument('--disable-bzip-memory', action='store_true', help='Don\'t zip the memory file. Not recommended (zipping is a bit slower and much, much smaller)')
 
-# Setup
-args = parser.parse_args()
-
-print(' ' * 26 + 'Options')
-for k, v in vars(args).items():
-  print(' ' * 26 + k + ': ' + str(v))
-results_dir = os.path.join('results', args.id)
-if not os.path.exists(results_dir):
-  os.makedirs(results_dir)
-metrics = {'steps': [], 'rewards': [], 'Qs': [], 'best_avg_reward': -float('inf')}
-np.random.seed(args.seed)
-torch.manual_seed(np.random.randint(1, 10000))
-if torch.cuda.is_available() and not args.disable_cuda:
-  args.device = torch.device('cuda')
-  torch.cuda.manual_seed(np.random.randint(1, 10000))
-  torch.backends.cudnn.enabled = args.enable_cudnn
-else:
-  args.device = torch.device('cpu')
-
 
 # Simple ISO 8601 timestamped logger
 def log(s):
@@ -99,87 +80,112 @@ def save_memory(memory, memory_path, disable_bzip):
       pickle.dump(memory, zipped_pickle_file)
 
 
-# Environment
-env = Env(args)
-env.train()
-action_space = env.action_space()
+def main(args=None):
+  # Setup
+  if args is None:
+    args = parser.parse_args()
 
-# Agent
-dqn = Agent(args, env)
+  print(' ' * 26 + 'Options')
+  for k, v in vars(args).items():
+    print(' ' * 26 + k + ': ' + str(v))
+  results_dir = os.path.join(os.path.dirname(__file__), 'results', args.id)
+  if not os.path.exists(results_dir):
+    os.makedirs(results_dir)
+  metrics = {'steps': [], 'rewards': [], 'Qs': [], 'best_avg_reward': -float('inf')}
+  np.random.seed(args.seed)
+  torch.manual_seed(np.random.randint(1, 10000))
+  if torch.cuda.is_available() and not args.disable_cuda:
+    args.device = torch.device('cuda')
+    torch.cuda.manual_seed(np.random.randint(1, 10000))
+    torch.backends.cudnn.enabled = args.enable_cudnn
+  else:
+    args.device = torch.device('cpu')
 
-# If a model is provided, and evaluate is fale, presumably we want to resume, so try to load memory
-if args.model is not None and not args.evaluate:
-  if not args.memory:
-    raise ValueError('Cannot resume training without memory save path. Aborting...')
-  elif not os.path.exists(args.memory):
-    raise ValueError('Could not find memory file at {path}. Aborting...'.format(path=args.memory))
+  # Environment
+  env = Env(args)
+  env.train()
+  action_space = env.action_space()
 
-  mem = load_memory(args.memory, args.disable_bzip_memory)
+  # Agent
+  dqn = Agent(args, env)
 
-else:
-  mem = ReplayMemory(args, args.memory_capacity)
+  # If a model is provided, and evaluate is false, presumably we want to resume, so try to load memory
+  if args.model is not None and not args.evaluate:
+    if not args.memory:
+      raise ValueError('Cannot resume training without memory save path. Aborting...')
+    elif not os.path.exists(args.memory):
+      raise ValueError('Could not find memory file at {path}. Aborting...'.format(path=args.memory))
 
-priority_weight_increase = (1 - args.priority_weight) / (args.T_max - args.learn_start)
+    mem = load_memory(args.memory, args.disable_bzip_memory)
+
+  else:
+    mem = ReplayMemory(args, args.memory_capacity)
+
+  priority_weight_increase = (1 - args.priority_weight) / (args.T_max - args.learn_start)
 
 
-# Construct validation memory
-val_mem = ReplayMemory(args, args.evaluation_size)
-T, done = 0, True
-while T < args.evaluation_size:
-  if done:
-    state, done = env.reset(), False
-
-  next_state, _, done = env.step(np.random.randint(0, action_space))
-  val_mem.append(state, None, None, done)
-  state = next_state
-  T += 1
-
-if args.evaluate:
-  dqn.eval()  # Set DQN (online network) to evaluation mode
-  avg_reward, avg_Q = test(args, 0, dqn, val_mem, metrics, results_dir, evaluate=True)  # Test
-  print('Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
-else:
-  # Training loop
-  dqn.train()
+  # Construct validation memory
+  val_mem = ReplayMemory(args, args.evaluation_size)
   T, done = 0, True
-  for T in trange(1, args.T_max + 1):
+  while T < args.evaluation_size:
     if done:
       state, done = env.reset(), False
 
-    if T % args.replay_frequency == 0:
-      dqn.reset_noise()  # Draw a new set of noisy weights
+    next_state, _, done = env.step(np.random.randint(0, action_space))
+    val_mem.append(state, None, None, done)
+    state = next_state
+    T += 1
 
-    action = dqn.act(state)  # Choose an action greedily (with noisy weights)
-    next_state, reward, done = env.step(action)  # Step
-    if args.reward_clip > 0:
-      reward = max(min(reward, args.reward_clip), -args.reward_clip)  # Clip rewards
-    mem.append(state, action, reward, done)  # Append transition to memory
-
-    # Train and test
-    if T >= args.learn_start:
-      mem.priority_weight = min(mem.priority_weight + priority_weight_increase, 1)  # Anneal importance sampling weight β to 1
+  if args.evaluate:
+    dqn.eval()  # Set DQN (online network) to evaluation mode
+    avg_reward, avg_Q = test(args, 0, dqn, val_mem, metrics, results_dir, evaluate=True)  # Test
+    print('Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
+  else:
+    # Training loop
+    dqn.train()
+    T, done = 0, True
+    for T in trange(1, args.T_max + 1):
+      if done:
+        state, done = env.reset(), False
 
       if T % args.replay_frequency == 0:
-        dqn.learn(mem)  # Train with n-step distributional double-Q learning
+        dqn.reset_noise()  # Draw a new set of noisy weights
 
-      if T % args.evaluation_interval == 0:
-        dqn.eval()  # Set DQN (online network) to evaluation mode
-        avg_reward, avg_Q = test(args, T, dqn, val_mem, metrics, results_dir)  # Test
-        log('T = ' + str(T) + ' / ' + str(args.T_max) + ' | Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
-        dqn.train()  # Set DQN (online network) back to training mode
+      action = dqn.act(state)  # Choose an action greedily (with noisy weights)
+      next_state, reward, done = env.step(action)  # Step
+      if args.reward_clip > 0:
+        reward = max(min(reward, args.reward_clip), -args.reward_clip)  # Clip rewards
+      mem.append(state, action, reward, done)  # Append transition to memory
 
-        # If memory path provided, save it
-        if args.memory is not None:
-          save_memory(mem, args.memory, args.disable_bzip_memory)
+      # Train and test
+      if T >= args.learn_start:
+        mem.priority_weight = min(mem.priority_weight + priority_weight_increase, 1)  # Anneal importance sampling weight β to 1
 
-      # Update target network
-      if T % args.target_update == 0:
-        dqn.update_target_net()
+        if T % args.replay_frequency == 0:
+          dqn.learn(mem)  # Train with n-step distributional double-Q learning
 
-      # Checkpoint the network
-      if (args.checkpoint_interval != 0) and (T % args.checkpoint_interval == 0):
-        dqn.save(results_dir, 'checkpoint.pth')
+        if T % args.evaluation_interval == 0:
+          dqn.eval()  # Set DQN (online network) to evaluation mode
+          avg_reward, avg_Q = test(args, T, dqn, val_mem, metrics, results_dir)  # Test
+          log('T = ' + str(T) + ' / ' + str(args.T_max) + ' | Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
+          dqn.train()  # Set DQN (online network) back to training mode
 
-    state = next_state
+          # If memory path provided, save it
+          if args.memory is not None:
+            save_memory(mem, args.memory, args.disable_bzip_memory)
 
-env.close()
+        # Update target network
+        if T % args.target_update == 0:
+          dqn.update_target_net()
+
+        # Checkpoint the network
+        if (args.checkpoint_interval != 0) and (T % args.checkpoint_interval == 0):
+          dqn.save(results_dir, 'checkpoint.pth')
+
+      state = next_state
+
+  env.close()
+
+
+if __name__ == "__main__":
+  main()
