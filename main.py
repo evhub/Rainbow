a414584382ae3ed59f1e8e41a363f2e0958e0292
Evan Hubinger <evanjhub@gmail.com>
2020-01-22 15:00:45 -0800

    Make training interruptible

--- main.py
+++ main.py
@@ -81,10 +81,10 @@ def save_memory(memory, memory_path, disable_bzip):
 
 
 def main(args=None):
-  # Setup
   if args is None:
     args = parser.parse_args()
 
+  # Setup
   print(' ' * 26 + 'Options')
   for k, v in vars(args).items():
     print(' ' * 26 + k + ': ' + str(v))
@@ -106,85 +106,96 @@ def main(args=None):
   env.train()
   action_space = env.action_space()
 
-  # Agent
-  dqn = Agent(args, env)
+  try:
+    # Agent
+    dqn = Agent(args, env)
 
-  # If a model is provided, and evaluate is false, presumably we want to resume, so try to load memory
-  if args.model is not None and not args.evaluate:
-    if not args.memory:
-      raise ValueError('Cannot resume training without memory save path. Aborting...')
-    elif not os.path.exists(args.memory):
-      raise ValueError('Could not find memory file at {path}. Aborting...'.format(path=args.memory))
+    # If a model is provided, and evaluate is false, presumably we want to resume, so try to load memory
+    if args.model is not None and not args.evaluate:
+      if not args.memory:
+        raise ValueError('Cannot resume training without memory save path. Aborting...')
+      elif not os.path.exists(args.memory):
+        raise ValueError('Could not find memory file at {path}. Aborting...'.format(path=args.memory))
 
-    mem = load_memory(args.memory, args.disable_bzip_memory)
+      mem = load_memory(args.memory, args.disable_bzip_memory)
 
-  else:
-    mem = ReplayMemory(args, args.memory_capacity)
+    else:
+      mem = ReplayMemory(args, args.memory_capacity)
 
-  priority_weight_increase = (1 - args.priority_weight) / (args.T_max - args.learn_start)
+    priority_weight_increase = (1 - args.priority_weight) / (args.T_max - args.learn_start)
 
-
-  # Construct validation memory
-  val_mem = ReplayMemory(args, args.evaluation_size)
-  T, done = 0, True
-  while T < args.evaluation_size:
-    if done:
-      state, done = env.reset(), False
-
-    next_state, _, done = env.step(np.random.randint(0, action_space))
-    val_mem.append(state, None, None, done)
-    state = next_state
-    T += 1
-
-  if args.evaluate:
-    dqn.eval()  # Set DQN (online network) to evaluation mode
-    avg_reward, avg_Q = test(args, 0, dqn, val_mem, metrics, results_dir, evaluate=True)  # Test
-    print('Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
-  else:
-    # Training loop
-    dqn.train()
+    # Construct validation memory
+    val_mem = ReplayMemory(args, args.evaluation_size)
     T, done = 0, True
-    for T in trange(1, args.T_max + 1):
+    while T < args.evaluation_size:
       if done:
         state, done = env.reset(), False
 
-      if T % args.replay_frequency == 0:
-        dqn.reset_noise()  # Draw a new set of noisy weights
-
-      action = dqn.act(state)  # Choose an action greedily (with noisy weights)
-      next_state, reward, done = env.step(action)  # Step
-      if args.reward_clip > 0:
-        reward = max(min(reward, args.reward_clip), -args.reward_clip)  # Clip rewards
-      mem.append(state, action, reward, done)  # Append transition to memory
-
-      # Train and test
-      if T >= args.learn_start:
-        mem.priority_weight = min(mem.priority_weight + priority_weight_increase, 1)  # Anneal importance sampling weight β to 1
-
-        if T % args.replay_frequency == 0:
-          dqn.learn(mem)  # Train with n-step distributional double-Q learning
-
-        if T % args.evaluation_interval == 0:
-          dqn.eval()  # Set DQN (online network) to evaluation mode
-          avg_reward, avg_Q = test(args, T, dqn, val_mem, metrics, results_dir)  # Test
-          log('T = ' + str(T) + ' / ' + str(args.T_max) + ' | Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
-          dqn.train()  # Set DQN (online network) back to training mode
-
-          # If memory path provided, save it
-          if args.memory is not None:
-            save_memory(mem, args.memory, args.disable_bzip_memory)
-
-        # Update target network
-        if T % args.target_update == 0:
-          dqn.update_target_net()
-
-        # Checkpoint the network
-        if (args.checkpoint_interval != 0) and (T % args.checkpoint_interval == 0):
-          dqn.save(results_dir, 'checkpoint.pth')
-
+      next_state, _, done = env.step(np.random.randint(0, action_space))
+      val_mem.append(state, None, None, done)
       state = next_state
+      T += 1
 
-  env.close()
+    if args.evaluate:
+      dqn.eval()  # Set DQN (online network) to evaluation mode
+      avg_reward, avg_Q = test(args, 0, dqn, val_mem, metrics, results_dir, evaluate=True)  # Test
+      print('Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
+
+    else:
+
+      # Training loop
+      dqn.train()
+      try:
+
+        T, done = 0, True
+        for T in trange(1, args.T_max + 1):
+          if done:
+            state, done = env.reset(), False
+
+          if T % args.replay_frequency == 0:
+            dqn.reset_noise()  # Draw a new set of noisy weights
+
+          action = dqn.act(state)  # Choose an action greedily (with noisy weights)
+          next_state, reward, done = env.step(action)  # Step
+          if args.reward_clip > 0:
+            reward = max(min(reward, args.reward_clip), -args.reward_clip)  # Clip rewards
+          mem.append(state, action, reward, done)  # Append transition to memory
+
+          # Train and test
+          if T >= args.learn_start:
+            mem.priority_weight = min(mem.priority_weight + priority_weight_increase, 1)  # Anneal importance sampling weight β to 1
+
+            if T % args.replay_frequency == 0:
+              dqn.learn(mem)  # Train with n-step distributional double-Q learning
+
+            if T % args.evaluation_interval == 0:
+              dqn.eval()  # Set DQN (online network) to evaluation mode
+              avg_reward, avg_Q = test(args, T, dqn, val_mem, metrics, results_dir)  # Test
+              log('T = ' + str(T) + ' / ' + str(args.T_max) + ' | Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
+              dqn.train()  # Set DQN (online network) back to training mode
+
+              # If memory path provided, save it
+              if args.memory is not None:
+                save_memory(mem, args.memory, args.disable_bzip_memory)
+
+            # Update target network
+            if T % args.target_update == 0:
+              dqn.update_target_net()
+
+            # Checkpoint the network
+            if args.checkpoint_interval != 0 and T % args.checkpoint_interval == 0:
+              dqn.save(results_dir, 'checkpoint.pth')
+
+          state = next_state
+
+      except KeyboardInterrupt:
+        print("Got KeyboardInterrupt; saving memory and model.")
+        if args.memory is not None:
+          save_memory(mem, args.memory, args.disable_bzip_memory)
+        dqn.save(results_dir, "interrupt.pth")
+
+  finally:
+    env.close()
 
 
 if __name__ == "__main__":
